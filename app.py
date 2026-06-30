aca2aa04b4377eba78d5da24a64e057f179eea27
D2Holly <holly.anscombe@digi2al.co.uk>
2026-06-30 11:17:14 +0100

    push latest changes

---
+++ app.py
@@ -0,0 +1,56 @@
+"""
+Docstring for app
+"""
+import time
+import requests
+
+def main():
+    """
+    Docstring for main
+    """
+    url = "https://api.crossref.org/journals/2072-4292/works"
+    params = {
+        "filter": "type:journal-article",
+        "sort": "published",
+        "order": "desc",
+        "select": "title,resource,volume,issue",
+        "rows": 30
+    }
+    headers = {
+        # crossref requires an email
+        "User-Agent": "MyDataScienceProject/1.0 (mailto:your-email@example.com)" 
+    }
+
+    response = requests.get(url, params=params, headers=headers, timeout=30)
+    if response.status_code == 200:
+        data = response.json()
+        items = data['message']['items']
+        if not items:
+            print("No data found")
+            return
+
+        for item in data['message']['items']:
+            #get the resource link of the current article
+            resource_dict = item.get('resource', {})
+            #get the primary url link of the current article's resource
+            primary = resource_dict.get('primary', {})
+            #
+            url_link = primary.get('URL', 'No Resource URL Available')
+
+            #get the volume no. of the current article. If it doesnt exist replace with ''
+            item_volume = str(item.get('volume', ''))
+
+
+            print(f"Title: {item['title'][0]}")
+            print(f"URL: {url_link}")
+            print(f"Volume: {item_volume}")
+            print(f"Issue: {item["issue"]}\n")
+
+            time.sleep(1)
+
+    else:
+        print("An error occured")
+        print(response.status_code)
+
+if __name__ == "__main__":
+    main()
