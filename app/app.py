1b63fea9e68417abd0efa8233704a92d3d70b55d
D2Holly <holly.anscombe@digi2al.co.uk>
2026-07-01 13:56:23 +0100

    broke app.py up into functions for testing, create test function for github actions and another for app.main()

--- app/app.py
+++ app/app.py
@@ -4,13 +4,70 @@ Docstring for app
 import time
 import requests
 
+
 def sample_tests(number):
+    """
+    Docstring for sample_tests
+    
+    :param number: Description
+    """
     if number == 1:
         return True
     elif number == 0:
         return False
     else:
         return False
+
+
+def get_response_data(url, params, headers):
+    """
+    Docstring for get_response_data
+    
+    :param url: Description
+    :param params: Description
+    :param headers: Description
+    """
+    response = requests.get(url, params=params, headers=headers, timeout=30)
+    if response.status_code == 200:
+        data = response.json()
+        items = data['message']['items']
+        if not items:
+            print("No data found")
+            return False
+
+        output_response_data(data)
+        return True
+
+    else:
+        print("An error occured")
+        print(response.status_code)
+
+
+def output_response_data(data):
+    """
+    Docstring for output_response_data
+    
+    :param data: Description
+    """
+    for item in data['message']['items']:
+        #get the resource link of the current article
+        resource_dict = item.get('resource', {})
+        #get the primary url link of the current article's resource
+        primary = resource_dict.get('primary', {})
+        #
+        url_link = primary.get('URL', 'No Resource URL Available')
+
+        #get the volume no. of the current article. If it doesnt exist replace with ''
+        item_volume = str(item.get('volume', ''))
+
+
+        print(f"Title: {item['title'][0]}")
+        print(f"URL: {url_link}")
+        print(f"Volume: {item_volume}")
+        print(f"Issue: {item["issue"]}\n")
+
+        time.sleep(1)
+    return True
 
 def main():
     """
@@ -29,36 +86,9 @@ def main():
         "User-Agent": "MyDataScienceProject/1.0 (mailto:your-email@example.com)" 
     }
 
-    response = requests.get(url, params=params, headers=headers, timeout=30)
-    if response.status_code == 200:
-        data = response.json()
-        items = data['message']['items']
-        if not items:
-            print("No data found")
-            return
+    result = get_response_data(url, params, headers)
+    return result
 
-        for item in data['message']['items']:
-            #get the resource link of the current article
-            resource_dict = item.get('resource', {})
-            #get the primary url link of the current article's resource
-            primary = resource_dict.get('primary', {})
-            #
-            url_link = primary.get('URL', 'No Resource URL Available')
-
-            #get the volume no. of the current article. If it doesnt exist replace with ''
-            item_volume = str(item.get('volume', ''))
-
-
-            print(f"Title: {item['title'][0]}")
-            print(f"URL: {url_link}")
-            print(f"Volume: {item_volume}")
-            print(f"Issue: {item["issue"]}\n")
-
-            time.sleep(1)
-
-    else:
-        print("An error occured")
-        print(response.status_code)
 
 if __name__ == "__main__":
     main()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,99 @@
 """
 a
 """
+import time
+import pytest
+import requests
 from app import app
 
 class TestApp:
     """
     Docstring for TestApp
     """
-    def test_tests(self):
+    @pytest.mark.parametrize(
+    "arguments",
+        [
+            1,
+            0,
+            "Banana"
+        ]
+    )
+    def test_ci_pipeline(self, arguments):
         """
         Docstring for test_try
         
         :param self: Description
         """
-        app.sample_tests(1)
+        if arguments == 1:
+            assert app.sample_tests(arguments) is True
+        elif arguments == 0:
+            assert app.sample_tests(arguments) is False
+        else:
+            assert app.sample_tests(arguments) is False
+
+
+#helper class for TestMain
+class MockResponse:
+    """
+    Helper class for TestMain which patches the response data of requests.get and
+    response.json
+    """
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        """
+        forces json to always return the given reponse code when MockResponse was called
+        (MockResponse(data, 200) will always return 200)
+        """
+        return self.json_data
+
+
+class TestMain:
+    """
+    Assigned tests for testing main in app/app.py
+    """
+    @pytest.mark.parametrize(
+    "exception_type",
+        [
+            200,
+            403
+        ]
+    )
+    def test_main(self, monkeypatch: pytest.MonkeyPatch, exception_type):
+        """
+        Docstring for test_main_happy
+        
+        :param self: Description
+        """
+        #fake response data in the format that we would expect requests.get(url, params=params,
+        # headers=headers, timeout=30) to return
+        mock_data = {
+            "message": {
+                "items": [
+                    {
+                        "title": ["Test Article"],
+                        "volume": "18",
+                        "issue": "1",
+                        "resource": {
+                            "primary": {
+                                "URL": "https://api.crossref.org/test"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+        def mock_get(*args, **kwargs): # pylint: disable=W0613
+            return MockResponse(mock_data, exception_type)
+
+        #mock requests.get into always returning our mock_data and giving a 200 response
+        monkeypatch.setattr(requests, 'get', mock_get)
+        #mock time.sleep into skipping the pauses for an instant response
+        monkeypatch.setattr(time, 'sleep', lambda x: None)
+
+        if exception_type == 200:
+            assert app.main() is True
+        else:
+            assert app.main() is None
